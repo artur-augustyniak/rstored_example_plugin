20f115d71c890e1924e5387aa49a900ea0124f42
Artur Augustyniak <artur@aaugustyniak.pl>
2017-04-22 16:52:07 +0200

    basic form

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 #[no_mangle]
-pub fn hosts() -> String {
-    let ret = r#"{ "file-name":"etc/hosts", "len":31, "content":"[aaugustyniak@bender somelib]$ cat /etc/hosts
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 bender
-::1         localhost localhost.localdomain localhost6 localhost6.localdomain6 bender
-
-#VIRT
-192.168.122.3   docker-host
-192.168.122.4   valkyrie
-" }"#.to_string();
-ret
+pub fn run_probe() -> String {
+    //your stuff, result must be encoded in k:v json string
+    let ret = r#"{ "key":"value", "len":31, "foo":"bar"}"#.to_string();
+    ret
 }
-
 
 #[cfg(test)]
 mod tests {
