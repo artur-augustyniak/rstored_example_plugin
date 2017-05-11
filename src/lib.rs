a29563233c08caac7f4bd1437f5a9a8a7f475e71
Artur Augustyniak <artur@aaugustyniak.pl>
2017-05-11 18:14:51 +0200

    c call conv and mangling

--- src/lib.rs
+++ src/lib.rs
@@ -4,32 +4,48 @@ use libc::c_char;
 use libc::puts;
 use std::ffi::CString;
 
-#[no_mangle]
-pub extern fn ctor() {
+
+#[allow(dead_code)]
+extern "C"
+fn ctor() {
     let s = CString::new("Loading Rust plugin!").unwrap();
     unsafe {
         let _ = puts(s.as_ptr());
     }
+    println!("Rust plugin println! macro in ctor.");
 }
 
-#[no_mangle]
-pub extern fn dtor() {
+#[allow(dead_code)]
+extern "C"
+fn dtor() {
     let s = CString::new("Unloading Rust plugin!").unwrap();
     unsafe {
         let _ = puts(s.as_ptr());
     }
+    println!("Rust plugin println! macro in dtor.");
 }
 
+
+#[cfg(not(test))]
+#[allow(dead_code)]
 #[link_section = ".ctors"]
-pub static CTOR: extern fn() = ctor;
+static CTOR: extern "C" fn() = ctor;
 
+#[cfg(not(test))]
+#[allow(dead_code)]
 #[link_section = ".dtors"]
-pub static DTOR: extern fn() = dtor;
+static DTOR: extern "C" fn() = dtor;
 
-static STR: &'static str = "Hello Rust plugin!";
+static STR: &'static str = "Hello Rust plugin!w";
 
 #[no_mangle]
-pub fn run_probe() -> *const c_char {
+pub  extern "C" fn run_probe() -> *const c_char {
+
+    println!("Rust plugin println! macro in run_probe.");
+    let local_s = CString::new("Rust plugin clib::puts in run_probe.").unwrap();
+    unsafe {
+        let _ = puts(local_s.as_ptr());
+    }
     let s = CString::new(STR).unwrap();
     return s.into_raw();
 }
